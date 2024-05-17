a16c940f0572793bb0b0ef1a86b5b8ea379fbd0a
julienkolani <julienkolani0@gmail.com>
2024-05-17 18:37:26 +0000

    add service

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ PYTHON_SCRIPT_PATH="$INSTALL_DIR/main.py"
 VIRTUAL_ENV_PATH="$INSTALL_DIR/venv"
 SERVICE_NAME="sensei_nakama"
 LOG_IDENTIFIER="sensei_nakama"
-PYTHON_MODULES="'configparser==7.0.0' 'pip==24.0' 'rocket-python==1.3.4'"
+PYTHON_MODULES="'configparser==7.0.0' 'rocket-python==1.3.4'"
 
 # Check if the script is running with root privileges
 if [ "$(id -u)" -ne 0 ]; then
@@ -34,19 +34,19 @@ install_dependencies() {
     if command_exists apt-get; then
         echo "Installing dependencies via apt-get..."
         apt-get update
-        apt-get install -y python3 python3-pip python-virtualenv git
+        apt-get install -y python3 python3-pip git
     elif command_exists dnf; then
         echo "Installing dependencies via dnf..."
-        dnf install -y python3 python3-pip python-virtualenv git
+        dnf install -y python3 python3-pip git
     elif command_exists yum; then
         echo "Installing dependencies via yum..."
-        yum install -y python3 python3-pip python-virtualenv git
+        yum install -y python3 python3-pip git
     elif command_exists pacman; then
         echo "Installing dependencies via pacman..."
-        pacman -Sy --noconfirm python python-pip python-virtualenv git
+        pacman -Sy --noconfirm python python-pip git
     elif command_exists zypper; then
         echo "Installing dependencies via zypper..."
-        zypper install -y python3 python3-pip python-virtualenv git
+        zypper install -y python3 python3-pip git
     else
         echo "No compatible package manager found. Please install Python, pip, and Git manually."
         exit 1
@@ -81,15 +81,16 @@ fi
 # Create the virtual environment and install dependencies
 if [ ! -d "$VIRTUAL_ENV_PATH" ]; then
     echo "Creating the virtual environment and installing the dependencies..."
+    pip3 install virtualenv 
     python3 -m venv "$VIRTUAL_ENV_PATH"
 fi
 
 # Activate the virtual environment and install/update dependencies
 echo "Activating virtual environment and installing/updating dependencies..."
 source "$VIRTUAL_ENV_PATH/bin/activate"
-pip install --upgrade pip
+"$VIRTUAL_ENV_PATH"/python3 -m pip install --upgrade pip
 for module in $PYTHON_MODULES; do
-    pip install "$module"
+     "$VIRTUAL_ENV_PATH"/python3 -m pip install "$module"
 done
 deactivate
 
