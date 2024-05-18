e9328f83faf3914e0c79eb52085fdaad47c3c099
julienkolani <julienkolani0@gmail.com>
2024-05-18 08:15:58 +0000

    add venv

--- install.sh
+++ install.sh
@@ -4,13 +4,19 @@ set -euo pipefail
 
 # Variables
 GIT_REPO_URL="https://github.com/julienkolani/sensei_nakama.git"
-INSTALL_DIR=/usr/bin/sensei_nakama
-CONFIG_DIR=/etc/sensei_nakama
+INSTALL_DIR="/usr/bin/sensei_nakama"
+CONFIG_DIR="/etc/sensei_nakama"
 PYTHON_SCRIPT_PATH="$INSTALL_DIR/main.py"
 VIRTUAL_ENV_PATH="$INSTALL_DIR/venv"
 SERVICE_NAME="sensei_nakama"
 LOG_IDENTIFIER="sensei_nakama"
-PYTHON_MODULES="'configparser==7.0.0' 'rocket-python==1.3.4'"
+POETRY_URL="https://install.python-poetry.org"
+
+# Option verbose
+VERBOSE=0
+if [[ "${1:-}" == "--verbose" ]]; then
+    VERBOSE=1
+fi
 
 # Check if the script is running with root privileges
 if [ "$(id -u)" -ne 0 ]; then
@@ -33,20 +39,20 @@ command_exists() {
 install_dependencies() {
     if command_exists apt-get; then
         echo "Installing dependencies via apt-get..."
-        apt-get update
-        apt-get install -y python3 python3-pip git
+        apt-get update -y
+        apt-get install -y python3 python3-pip git curl
     elif command_exists dnf; then
         echo "Installing dependencies via dnf..."
-        dnf install -y python3 python3-pip git
+        dnf install -y python3 python3-pip git curl
     elif command_exists yum; then
         echo "Installing dependencies via yum..."
-        yum install -y python3 python3-pip git
+        yum install -y python3 python3-pip git curl
     elif command_exists pacman; then
         echo "Installing dependencies via pacman..."
-        pacman -Sy --noconfirm python3 python3-pip git
+        pacman -Sy --noconfirm python3 python3-pip git curl
     elif command_exists zypper; then
         echo "Installing dependencies via zypper..."
-        zypper install -y python3 python3-pip git
+        zypper install -y python3 python3-pip git curl
     else
         echo "No compatible package manager found. Please install Python, pip, and Git manually."
         exit 1
@@ -54,7 +60,14 @@ install_dependencies() {
 }
 
 # Install necessary dependencies
-install_dependencies
+if [[ $VERBOSE -eq 1 ]]; then
+    echo "Installing dependencies ..."
+    install_dependencies
+else
+    set +x
+    install_dependencies
+    set -x
+fi
 
 # Create the directory for the program and copy the files
 echo "Creating the directory for the program and copying the files..."
@@ -62,8 +75,8 @@ echo "Creating the directory for the program and copying the files..."
 # Clone or update the Git repository
 if [ -d "$INSTALL_DIR" ]; then
     echo "Directory $INSTALL_DIR already exists. Updating repository..."
-    cd "$INSTALL_DIR"
-    git pull
+    rm -rf "$INSTALL_DIR"
+    git clone "$GIT_REPO_URL" "$INSTALL_DIR"
 else
     echo "Cloning the repository from Git..."
     git clone "$GIT_REPO_URL" "$INSTALL_DIR"
@@ -78,21 +91,21 @@ else
     echo "Configuration directory $CONFIG_DIR already exists. Skipping configuration file copy."
 fi
 
-# Create the virtual environment and install dependencies
-if [ ! -d "$VIRTUAL_ENV_PATH" ]; then
-    echo "Creating the virtual environment and installing the dependencies..."
-    pip3 install virtualenv 
-    python3 -m venv "$VIRTUAL_ENV_PATH"
+# Install Poetry if not installed
+if ! command_exists poetry; then
+    echo "Installing Poetry..."
+    curl -sSL $POETRY_URL | python3 -
+    export PATH="$HOME/.local/bin:$PATH"
 fi
 
-# Activate the virtual environment and install/update dependencies
-echo "Activating virtual environment and installing/updating dependencies..."
-source "$VIRTUAL_ENV_PATH/bin/activate"
-"$VIRTUAL_ENV_PATH"/python3 -m pip install --upgrade pip
-for module in $PYTHON_MODULES; do
-     "$VIRTUAL_ENV_PATH"/python3 -m pip install "$module"
-done
-deactivate
+# Install/update dependencies using Poetry
+echo "Activating virtual environment and installing/updating dependencies with Poetry..."
+if [ ! -d "$VIRTUAL_ENV_PATH" ]; then
+    mkdir -p "$VIRTUAL_ENV_PATH"
+fi
+pushd "$INSTALL_DIR" > /dev/null
+poetry install
+popd > /dev/null
 
 # Create Systemd service file
 echo "Creating Systemd service file..."
@@ -102,7 +115,7 @@ Description=$SERVICE_NAME Service
 After=network.target
 
 [Service]
-ExecStart=$VIRTUAL_ENV_PATH/bin/python3 $PYTHON_SCRIPT_PATH
+ExecStart=$(poetry run which python) $PYTHON_SCRIPT_PATH
 Restart=always
 User=root
 Group=root
@@ -125,7 +138,6 @@ sudo systemctl status "$SERVICE_NAME"
 
 # Remove the cloned directory if necessary
 echo "Removing the cloned Git repository directory..."
-
-rm -rf sensei_nakama
+rm -rf "$INSTALL_DIR"
 
 echo "Installation complete."
